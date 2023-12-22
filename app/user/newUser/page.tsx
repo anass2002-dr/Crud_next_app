433e7b70e01bdd627a20c39304dd8549bf730ea1
anass dermaj <anassdermaj163@gmail.com>
2023-12-22 17:55:41 +0100

    22-12-2023-18:55

---
+++ app/user/newUser/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+function newUser(){
+    return(
+        <div>
+            <h1>this is new newUser</h1>
+        </div>
+    )
+}
+export default newUser;
---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import header_app from "../components/header";
+const user=()=>{
+    return(
+        
+        <div>
+            <h1>this is user</h1>
+            {/* <header_app/> */}
+        </div>
+        
+    )
+}
+export default user;
